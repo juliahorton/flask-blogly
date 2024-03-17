d0453095e0471c339d39530ef6731ce74d7fe69f
Julia Horton <juliahorton00@gmail.com>
2024-03-17 01:44:07 -0500

    Modify app.py and models.py

--- app.py
+++ app.py
@@ -1,12 +1,99 @@
-"""Blogly application."""
+"""Blogly Flask application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+# don't forget to import all models by name later
 
 app = Flask(__name__)
+# app.app_context().push()
+
+app.config['SECRET_KEY'] = 'itsasecret'
+debug = DebugToolbarExtension(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+
+# db.create_all()
+
+@app.route("/")
+def home_page():
+        """Redirect to list of users."""
+        
+        return redirect("/users")
+    
+
+@app.route("/users")
+def show_users():
+        """Show all users. Make these links to view the detail page for the user. Have a link here to the add-user form."""
+    
+        users = User.query.order_by(User.last_name, User.first_name).all()
+
+        return render_template("users/index.html", users=users)
+    
+
+@app.route("/users/new", methods=["GET"])
+def show_add_form():
+    """Show an add form for users."""
+
+    return render_template("/users/new.html")
+    
+
+@app.route("/users/new", methods=["POST"])
+def handle_add_form():
+    """Process the add form, adding a new user and going back to /users."""
+
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url or None)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>")
+def show_user_info(user_id):
+    """Show information about the given user. Have a button to get to their edit page, and to delete the user."""
+
+    user = User.query.get_or_404(user_id)
+
+    return render_template("users/show.html",user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def show_edit_page(user_id):
+    """Show the page for a user. Have a cancel button that returns to the detail page for a user, and a save button that updates the user."""
+
+    user = User.query.get_or_404(user_id)
+
+    return render_template("/users/edit.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def handle_edit_form(user_id):
+    """Process the edit form, returning the user to the /users page."""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form["first-name"]
+    user.last_name = request.form["last-name"]
+    user.image_url = request.form["image-url"]
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+    
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    """Delete the user."""
+
+    User.query.filter_by(id=user_id).delete()
+    db.session.commit()
+
+    return redirect("/users")
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+default_url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile-PNG-File.png&f=1&nofb=1&ipt=c6ad3ea13d612bf8902ee57a330331b9eb80c538bf44859b3db2269cdcbb7969&ipo=images"
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, default=default_url)
+
+    def __repr__(self):
+        """Show information about user."""
+        user = self
+        return f"<User {user.id} {user.first_name} {user.last_name}>"
+    
+    def full_name(self):
+        """Get user's full name."""
+        return f"{self.first_name} {self.last_name}"

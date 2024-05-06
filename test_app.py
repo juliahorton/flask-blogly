95ccfd2c3022e2ad44777f76a41f526c70a5c1f0
Julia Horton <juliahorton00@gmail.com>
2024-05-05 23:49:38 -0500

    added tests

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 # Use test database and don't clutter tests with SQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
@@ -22,15 +22,26 @@ class BloglyTestCase(TestCase):
     def setUp(self):
         """Add sample users"""
 
+        Post.query.delete()
         User.query.delete()
         
-        user = User(first_name="Michelle", last_name="Obama", image_url="https://www.dispatch.com/gcdn/authoring/2014/01/16/NCOD/ghows-OH-8dc0e22b-d75a-4408-b0fa-5195209baa1b-25e25e82.jpeg?width=1200&disable=upscale&format=pjpg&auto=webp")
+        user1 = User(first_name="Michelle", last_name="Obama", image_url="https://www.dispatch.com/gcdn/authoring/2014/01/16/NCOD/ghows-OH-8dc0e22b-d75a-4408-b0fa-5195209baa1b-25e25e82.jpeg?width=1200&disable=upscale&format=pjpg&auto=webp")
 
-        db.session.add(user)
+        user2 = User(first_name="Elizabeth", last_name="Woolridge")
+
+        db.session.add_all([user1, user2])
         db.session.commit()
 
-        self.user_id = user.id
-        self.user = user
+        self.user1 = user1
+        self.user2 = user2
+
+        post = Post(title="A Note from Michelle Obama", content="School lunches are going to get soooooooo much worse but it's okay because they'll actually be soooooo much better and you will be healthy and thank me. Thumbs up", user_id=self.user1.id)
+
+        db.session.add(post)
+        db.session.commit()
+
+        self.post_id = post.id
+        self.post = post
 
     def test_home_page(self):
         with app.test_client() as client:
@@ -49,18 +60,32 @@ class BloglyTestCase(TestCase):
 
     def test_show_user_info(self):
         with app.test_client() as client:
-            resp = client.get(f"/users/{self.user_id}")
+            resp = client.get(f"/users/{self.user1.id}")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Michelle Obama", html)
             self.assertIn("Edit", html)
             self.assertIn("Delete", html)
+            self.assertIn("A Note from Michelle Obama", html)
 
     def test_delete_user(self):
         with app.test_client() as client:
-            resp = client.post(f"/users/{self.user_id}/delete")
+            resp = client.post(f"/users/{self.user2.id}/delete")
 
-            self.assertNotIn(User.query.get(self.user_id), User.query.all())
+            self.assertNotIn(User.query.get(self.user2.id), User.query.all())
 
             self.assertEqual(resp.status_code, 302)
+
+    def test_delete_post(self):
+        with app.test_client() as client:
+            resp = client.post(f"/posts/{self.post_id}/delete")
+
+            self.assertNotIn(Post.query.get(self.post_id), Post.query.all())
+
+            self.assertEqual(resp.status_code, 302)
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
